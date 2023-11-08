aee82f6eb191a3bd4281a904855ecf2a7af3ff9f
Shuubhii <126624347+Shuubhii@users.noreply.github.com>
2023-11-08 18:25:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("Let's create a band name")
+a=input("Whats your city name?\n")
+b=input("Whats your pet name\n")
+print("option a:",a,"",b)
+print("option b:",b,"",a)
